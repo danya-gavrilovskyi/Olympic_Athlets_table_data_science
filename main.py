5a88bcc4f2c6caee9176f4045da7704ab2fd8d55
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2022-12-21 14:46:29 +0200

    Changed main function

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import argparse
-
-def task1(filename, country, year):
-    head = None
-    is_first_line = True
-    with open(filename, 'r') as file:
-        for line in file.readlines():
-            data = line.strip().split('/t')
-            if is_first_line:
-                head = data
-                is_first_line = False
-                continue
-
-            if country == data[head.index('NOC')] and year == data[head.index('Year')]:
-                pass
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
     parser.add_argument('--medals', action='store_true', required=False)
     parser.add_argument('--total', action='store_true', required=False)
-    parser.add_argument('--filename', required=True)
+    parser.add_argument('--overall', nargs='*', required=False)
+    parser.add_argument('--interactive', action='store_true', required=False)
 
-    args = parser.parse_args()
-    
+    if sys.argv[2] == '--medals':
+        parser.add_argument('country')
+        parser.add_argument('year')
+        parser.add_argument('--output')
+        args = parser.parse_args()
+        task1(args.filename, args.country, args.year, args.output)
+    elif sys.argv[2] == '--total':
+        parser.add_argument('year')
+        args = parser.parse_args()
+        task2(args.filename, args.year)
+    elif sys.argv[2] == '--overall':
+        args = parser.parse_args()
+        task3(args.filename, args.overall)
+    elif sys.argv[2] == '--interactive':
+        args = parser.parse_args()
+        task4(args.filename)
