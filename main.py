20057d111d1a6a9e8612883b9e48affbfeccaaf3
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2022-12-21 14:59:54 +0200

    Task3 is ready

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import argparse
 
+def task3(filename, *args):
+    head = head_taker(filename)
+    years_and_medals = {}
+    for list in args:
+        for country in list:
+            years_and_medals.clear()
+            with open(filename, 'r') as file:
+                for line in file.readlines():
+                    data = line.strip().split('\t')
+                    if (data[head.index('Team')] == country or data[head.index('NOC')] == country) and data[head.index('Year')] not in years_and_medals and data[head.index('Medal')] != 'NA':
+                        years_and_medals[data[head.index('Year')]] = 1
+                    elif (data[head.index('Team')] == country or data[head.index('NOC')] == country) and data[head.index('Year')] in years_and_medals and data[head.index('Medal')] != 'NA':
+                        years_and_medals[data[head.index('Year')]] += 1
+
+            dict_values = [int(x) for x in years_and_medals.values()]
+            dict_keys = [int(x) for x in years_and_medals.keys()]
+            max_medals = max(dict_values)
+            print(f"The most medals {country} earned in {dict_keys[dict_values.index(max_medals)]}'s year and the number was {max_medals}")
+    return dict_values, dict_keys
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
